55bbc0042709e2d1cea35e0cfe7d8e2ea3b9d37f
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-06-21 12:30:37 +0200

    add card component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: { API_KEY: process.env.API_KEY },
+    // env: { API_KEY: process.env.API_KEY },
+    images: {
+        domains: ["image.tmdb.org"],
+    },
 };
 
 module.exports = nextConfig;
