70890feb9987e928cd07a4cf4561058eb71a18ea
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-06-21 11:16:27 +0200

    fetching data

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,36 @@
-export default function Home() {
-    return <h1 className="font-bold text-red-500">HOME</h1>;
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+    const genre = searchParams.genre || "fetchTrending";
+
+    const res = await fetch(
+        `https://api.themoviedb.org/3/${
+            genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+        }?api_key=212aa34a5e71fb2d80549e64932ff99c&language=en-US&page=1`,
+        { next: { revalidate: 10000 } },
+    );
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data!");
+    }
+
+    const data = await res.json();
+
+    const results = data.results;
+    // console.log(results); //per vedere se funzia
+
+    return (
+        <div className="">
+            <Results results={results} />
+        </div>
+    );
 }
+
+// const res = await fetch(
+//     genre === "fetchTopRated"
+//         ? `https://api.themoviedb.org/3/movie/top_rated?api_key=212aa34a5e71fb2d80549e64932ff99c&language=en-US&page=1`
+//         : `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`,
+//     { next: { revalidate: 10000 } },
+// );
